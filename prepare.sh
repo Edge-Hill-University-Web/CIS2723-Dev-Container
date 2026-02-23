3e1263272e9737c04f8d50fbd2244372b628ac76
Dan Campbell <danielcampbell2097@hotmail.com>
2026-02-23 16:15:22 +0000

    Update prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -3,4 +3,10 @@
 echo "Creating Module Directory Structure"
 /bin/bash -c "mkdir -p tutorials/{week01,week02,week03,week04,week05,week06,week07,week08,week09,week10,week11,week12}"
 /bin/bash -c "rm echo"
+
+EXT_DIR="$HOME/.vscode-server/extensions"
+if [ -d "$EXT_DIR" ]; then
+  rm -rf "$EXT_DIR"/github.copilot*
+fi
+
 exec "$@"
